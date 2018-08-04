eeed31959d3367953fcc76be8c82c7d11a76960f
Pablo <pablotrindade93@gmail.com>
2018-08-04 17:01:40 -0300

    criada estrutura inicial do projeto.

---
+++ serverapi/src/main/java/br/com/contasfacil/serverapi/ServerapiApplication.java
@@ -0,0 +1,12 @@
+package br.com.contasfacil.serverapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerapiApplication.class, args);
+	}
+}
